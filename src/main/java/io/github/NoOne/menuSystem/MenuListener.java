3b4841b882fa6b02879ddc46a1f832681bb1a1b6
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-08-08 23:37:55 -0400

    now its just simple debugging

--- src/main/java/io/github/NoOne/menuSystem/MenuListener.java
+++ src/main/java/io/github/NoOne/menuSystem/MenuListener.java
@@ -3,25 +3,24 @@ package io.github.NoOne.menuSystem;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.PlayerInventory;
 
 public class MenuListener implements Listener {
     @EventHandler
     public void onMenuClick(InventoryClickEvent event) {
-        if (event.getView().getTopInventory().getHolder() instanceof Menu menu && event.getClickedInventory() != null) {
-            System.out.println("sneed");
-            event.setCancelled(true);
-            if (event.getCurrentItem() == null) return;
+        Inventory topInventory = event.getView().getTopInventory();
+        Inventory clickedInventory = event.getClickedInventory();
 
-            menu.handleMenu(event);
-//            if (event.getClickedInventory().equals(event.getView().getTopInventory())) {
-//                System.out.println("sneed");
-//                event.setCancelled(true);
-//                if (event.getCurrentItem() == null) return;
-//
-//                menu.handleMenu(event);
-//            }
+        if (topInventory.getHolder() instanceof Menu menu && clickedInventory != null) {
+            if (clickedInventory.equals(topInventory)) {
+                System.out.println("sneed");
+                event.setCancelled(true);
+                if (event.getCurrentItem() == null) return;
+
+                menu.handleMenu(event);
+            }
         }
     }
 }
