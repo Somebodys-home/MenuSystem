da76d72138f72aa6db73e53e5e8fef7a6a883978
NoOne_NoBody <hypixelnerd347@gmail.com>
2026-03-01 20:58:10 -0500

    updated getplayermenuutility method

--- src/main/java/io/github/NoOne/menuSystem/MenuSystem.java
+++ src/main/java/io/github/NoOne/menuSystem/MenuSystem.java
@@ -1,5 +1,7 @@
 package io.github.NoOne.menuSystem;
 
+import org.bukkit.entity.Player;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -7,7 +9,9 @@ import java.util.UUID;
 public final class MenuSystem {
     private static final HashMap<UUID, PlayerMenuUtility> playerMenuUtilityMap = new HashMap<>();
 
-    public static PlayerMenuUtility getPlayerMenuUtility(UUID uuid) {
+    public static PlayerMenuUtility getPlayerMenuUtility(Player player) {
+        UUID uuid = player.getUniqueId();
+
         if (!playerMenuUtilityMap.containsKey(uuid)) playerMenuUtilityMap.put(uuid, new PlayerMenuUtility(uuid));
 
         return playerMenuUtilityMap.get(uuid);
